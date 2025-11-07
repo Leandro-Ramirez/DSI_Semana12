688d9697bc7e753739784d00d5a433730086dcd7
Leandro-Kun <90274325+Leandro-Ramirez@users.noreply.github.com>
2025-11-07 15:15:24 -0600

    Cam

--- script/app.js
+++ script/app.js
@@ -1,145 +1,157 @@
-const tabla = document.getElementById('tablaEstudiantes');
-const form = document.getElementById('formEstudiante');
-const idInput = document.getElementById('id');
-const nombreInput = document.getElementById('nombre');
-const apellidoInput = document.getElementById('apellido');
-const edadInput = document.getElementById('edad');
+const tabla = document.getElementById("tablaEstudiantes"); 
+const form = document.getElementById("formEstudiante"); 
+const idInput = document.getElementById("id");
+const nombreInput = document.getElementById("nombre"); 
+const apellidoInput = document.getElementById("apellido"); 
+const edadInput = document.getElementById("edad");
+
 
 async function obtenerEstudiantes() {
-    try {
-        const response = await fetch('https://dsi-strapi.onrender.com/api/estudiantes');
+    try{
+        const response = await fetch(`https://dsi-strapi.onrender.com/api/estudiantes`); 
         if (!response.ok) {
-            throw new Error('Error al obtener los estudiantes');
+            throw new Error('Network response was not ok');
         }
         const data = await response.json();
         console.log(data.data);
         return data.data;
-    } catch (error) {
+    }catch (error) {
         console.error('Error al obtener estudiantes:', error);
         return [];
-    }
+    }    
 }
 
-async function agregarEstudiante(estudiante) {
 
+
+async function agregarEstudiante(estudiante) { 
     try {
-        const response = await fetch('https://dsi-strapi.onrender.com/api/estudiantes', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'},
-            body: JSON.stringify({ data: estudiante })
-        });
+        const response = await fetch(`https://dsi-strapi.onrender.com/api/estudiantes`, { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: estudiante })
 
-        if (!response.ok) {
-            throw new Error('Error al agregar el estudiante');
-        }
+    });
+    if (!response.ok) {
+        throw new Error('Error al agregar el estudiante.');
+    }
 
-        await init(); // Recarcar la tabla
-        form.reset(); // Limpiar el formulario
-
+    await init(); // Recargar la tabla
+    form.reset(); // Limpiar el formulario
     } catch (error) {
-        console.error('Error al agregar Estudiante:', error);
+        console.error('Error en agregarEstudiante:', error);
     }
 }
 
-form.addEventListener('submit', async (e) => {
+
+form.addEventListener('submit', async (e) => { 
     e.preventDefault();
     const docId = idInput.value;
-    const nombre = nombreInput.value.trim();
-    const apellido = apellidoInput.value.trim();
+    const nombre = nombreInput.value.trim(); 
+    const apellido = apellidoInput.value.trim(); 
     const edad = edadInput.value.trim();
 
     if (!nombre || !apellido || !edad) {
-        alert('Por favor, complete todos los campos.');
+        alert('Por favor, complete todos los campos (Nombre, Apellido, Edad).');
         return;
     }
 
-    const estudianteData = {
+    const estudainteData = {
         Nombre: nombre,
         Apellido: apellido,
         edad: parseInt(edad, 10)
     };
 
-    if (docId) {
-        await actualizarEstudiante(docId, estudianteData);
-    } else {
-        await agregarEstudiante(estudianteData);
+    if(docId)
+    {
+        await actualizarEstudiante(docId, estudainteData);   
     }
-
+    else{
+        await agregarEstudiante(estudainteData);
+    }
     form.reset();
     await init();
 });
 
-function cargarTabla(estudiantes) {
-    tabla.innerHTML = '';
-    if (estudiantes.length === 0) {
-        tabla.innerHTML = '<tr><td colspan="5" class="text-center">No hay estudiantes registrados.</td></tr>';
-        return;
+async function eliminarEstudiante(docId){
+    try{
+        const response = await fetch(`https://dsi-strapi.onrender.com/api/estudiantes/${docId}`, {
+            method:'DELETE',
+        });
+        if(!response.ok) throw new Error ('Error al eliminar el estudiante.');
     }
-    estudiantes.forEach(est => {
-        const fila = `
-        <tr data-document-id="${est.documentId}">
-            <td>${est.id}</td>
-            <td>${est.Nombre}</td>
-            <td>${est.Apellido}</td>
-            <td>${est.edad}</td>
-            <td>
-                <button class="btn btn-sm btn-warning btn-editar">Editar</button>
-                <button class="btn btn-sm btn-danger btn-eliminar">Eliminar</button>
-            </td>
-        </tr>`;
-    tabla.innerHTML += fila;
-    });
-}
- async function init() {
-    estudiantes = await obtenerEstudiantes();
-    cargarTabla(estudiantes);
+    catch(error){
+        console.error('Error en eliminarEstudiante', error);
+    }
 }
 
-init();
 
-// Logica para poblar el formulario al editar
+
+
+// Lógica para poblar el formulario al editar 
 tabla.addEventListener('click', async (e) => {
-    if (e.target.classList.contains('btn-editar')) {
-        const fila = e.target.closest('tr');
-        const docId = fila.dataset.documentId
-        const nombre = fila.children[1].textContent;
-        const apellido = fila.children[2].textContent;
-        const edad = fila.children[3].textContent;
-        idInput.value = docId;
+    if (e.target.classList.contains('btn-editar')) { 
+        const fila = e.target.closest('tr');        
+        const docId = fila.dataset.documentId;
+        const nombre = fila.children [1].textContent; 
+        const apellido = fila.children [2].textContent; 
+        const edad = fila.children [3].textContent;
+        
+        idInput.value = docId; // Usamos el input oculto para guardar el documentId
         nombreInput.value = nombre;
         apellidoInput.value = apellido;
         edadInput.value = edad;
-    } else if (e.target.classList.contains('btn-eliminar')) {
-        const fila = e.target.closest('tr');
+    }
+    else if (e.target.classList.contains('btn-eliminar')){
+        const fila= e.target.closest('tr');
         const docId = fila.dataset.documentId;
-        if (confirm('¿Está seguro de que desea eliminar este estudiante?')) {
+        if(confirm('?Estas seguro de que quieres eliminar este estudiante?')) {
             await eliminarEstudiante(docId);
-            await init(); // Recargar la tabla despues de eliminar
+            await init(); //Recargar la tabla despues de eliminar
         }
     }
 });
 
-async function actualizarEstudiante(docId, estudiante) {
+
+
+async function actualizarEstudiante (docId, estudiante) {
     try {
-        const response = await fetch(`https://dsi-strapi.onrender.com/api/estudiantes/${documentId}`, {
+        const response = await fetch(`https://dsi-strapi.onrender.com/api/estudiantes/${docId}`,{  
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ data: estudiante })
         });
         if (!response.ok) throw new Error('Error al actualizar el estudiante.');
-        } catch (error) {
-        console.error('Error al actualizar Estudiante:', error);
+    }catch (error) {
+        console.error('Error en actualizarEstudiante:', error);
     }
 }
 
-async function eliminarEstudiante(docId) {
-    try {
-        const response = await fetch(`https://dsi-strapi.onrender.com/api/estudiantes/${docId}`, {
-            method: 'DELETE'
-        });
-        if (!response.ok) throw new Error('Error al eliminar el estudiante.');
-        } catch (error) {
-        console.error('Error al eliminar Estudiante:', error);
+function cargarTabla (estudiantes) { 
+    tabla.innerHTML = "";
+    if (estudiantes.length === 0) {
+        tabla.innerHTML = "<tr><td colspan='5' class='text-center'>No hay estudiantes registrados.</td></tr>";
+        return;
     }
+
+    estudiantes.forEach (est => {   
+        const fila =`
+            <tr data-document-id="${est.documentId}">
+                <td>${est.id}</td>
+                <td>${est.Nombre}</td>
+                <td>${est.Apellido}</td> <td>${est.edad}</td>
+                <td>
+                    <button class="btn btn-sm btn-warning btn-editar">Editar</button>
+                    <button class="btn btn-sm btn-danger btn-eliminar">Eliminar</button> 
+                </td>
+            
+            </tr>`;
+    tabla.innerHTML += fila;
+    });
 }
+
+async function init() {
+const estudiantes = await obtenerEstudiantes(); cargarTabla (estudiantes);
+}
+init();
